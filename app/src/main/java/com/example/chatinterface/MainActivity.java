f9b0b6bab7d8cb7776654e325d2482cfab2514ae
Mayyar <s6111302@gmail.com>
2019-07-12 17:39:55 +0800

    Firstcommit

---
+++ app/src/main/java/com/example/chatinterface/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.chatinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helper = (ImageButton)findViewById(R.id.ib_helper);
+        helper.setOnClickListener(doClick);
+    }
+
+    private Button.OnClickListener doClick = new Button.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = new Intent(MainActivity.this, ChatRoomActivity.class);
+            startActivity(intent);
+        }
+    };
+}
